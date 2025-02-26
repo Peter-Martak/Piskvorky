e51400d1c65ab93fcc8adad9f81d0c029a448e76
Peter <peter.martak1@gmail.com>
2025-02-26 18:51:19 +0100

    oprava kontrola miesta na hracej ploche

--- src/Main.java
+++ src/Main.java
@@ -21,38 +21,38 @@ public class Main {
         while (!vitaz) {
             int zadaneCislo;
             try {
-                    System.out.println("Na ťahu je hráč " + hrac +
-                            " prosím zadaj číslo od 1 - 9");
+                System.out.println("Na ťahu je hráč " + hrac +
+                        " prosím zadaj číslo od 1 - 9");
 
-                    // zadavam číslo od hraca a overujem či zadal cislo od 1-9
+                // zadavam číslo od hraca a overujem či zadal cislo od 1-9
+                zadaneCislo = scanner.nextInt();
+                while (zadaneCislo < 1 || zadaneCislo > 9) {
+                    System.out.println("musíš zadať číslo od 1 - 9");
                     zadaneCislo = scanner.nextInt();
-                    while (zadaneCislo < 1 || zadaneCislo > 9) {
-                        System.out.println("musíš zadať číslo od 1 - 9");
-                        zadaneCislo = scanner.nextInt();
-                    }
-                    //overujem ci dane miesto na hracej ploche je volne
-                    miestoVHre = vyberMiestoVPoli(zadaneCislo);
-                    if (hraciaPlocha[miestoVHre[0]][miestoVHre[1]] == 'x'
-                            || hraciaPlocha[miestoVHre[0]][miestoVHre[1]] == 'o'){
-                        System.out.println("toto miesto na hracej ploche je obsadené");
-                        System.out.println("zadaj iné číslo od 1-9");
-                        continue;
-                    }else{
-                        hraciaPlocha[miestoVHre[0]][miestoVHre[1]] = hraciZnak;
-                        vykresliPolePoVlozeniZnaku(hraciaPlocha);
-                    }
+                }
+                //overujem ci dane miesto na hracej ploche je volne
+                miestoVHre = vyberMiestoVPoli(zadaneCislo);
+                if (hraciaPlocha[miestoVHre[0]][miestoVHre[1]] == 'X'
+                        || hraciaPlocha[miestoVHre[0]][miestoVHre[1]] == 'O') {
+                    System.out.println("toto miesto na hracej ploche je obsadené");
+                    System.out.println("zadaj iné číslo od 1-9");
+                    continue;
+                } else {
+                    hraciaPlocha[miestoVHre[0]][miestoVHre[1]] = hraciZnak;
+                    vykresliPolePoVlozeniZnaku(hraciaPlocha);
+                }
 
-                    // overenie vitaza
-                    vitaz = overVitazaVRiadku(hraciaPlocha,hraciZnak,hrac,vitaz);
-                    vitaz = overVitazaVStlpci(hraciaPlocha,hraciZnak,hrac,vitaz);
-                    vitaz = overVitazaDiagonalne(hraciaPlocha,hraciZnak,hrac,vitaz);
+                // overenie vitaza
+                vitaz = overVitazaVRiadku(hraciaPlocha, hraciZnak, hrac, vitaz);
+                vitaz = overVitazaVStlpci(hraciaPlocha, hraciZnak, hrac, vitaz);
+                vitaz = overVitazaDiagonalne(hraciaPlocha, hraciZnak, hrac, vitaz);
 
                 //overenie remizy
-                if (!vitaz){
-                if (overRemizu(hraciaPlocha)) {
-                    System.out.println("Hra skončíla remízou");
-                    break;
-                }
+                if (!vitaz) {
+                    if (overRemizu(hraciaPlocha)) {
+                        System.out.println("Hra skončíla remízou");
+                        break;
+                    }
                 }
 
                 hrac = (hrac == 'X') ? 'O' : 'X';
@@ -66,7 +66,7 @@ public class Main {
         }
     }
 
-    public static boolean overVitazaDiagonalne(char[][] hraciaPlocha, char hraciZnak, char hrac, boolean vitaz){
+    public static boolean overVitazaDiagonalne(char[][] hraciaPlocha, char hraciZnak, char hrac, boolean vitaz) {
         if (hraciaPlocha[0][0] == hraciZnak && hraciaPlocha[1][1] == hraciZnak && hraciaPlocha[2][2] == hraciZnak
                 || hraciaPlocha[2][0] == hraciZnak && hraciaPlocha[1][1] == hraciZnak && hraciaPlocha[0][2] == hraciZnak) {
             System.out.println("Vyhral hráč: " + hrac);
@@ -75,7 +75,7 @@ public class Main {
         return vitaz;
     }
 
-    public static boolean overVitazaVStlpci(char[][] hraciaPlocha, char hraciZnak, char hrac, boolean vitaz){
+    public static boolean overVitazaVStlpci(char[][] hraciaPlocha, char hraciZnak, char hrac, boolean vitaz) {
         for (int i = 0; i < hraciaPlocha.length; i++) {
             if (hraciaPlocha[0][i] == hraciZnak && hraciaPlocha[1][i] == hraciZnak && hraciaPlocha[2][i] == hraciZnak) {
                 System.out.println("Vyhral hráč: " + hrac);
@@ -85,7 +85,7 @@ public class Main {
         return vitaz;
     }
 
-    public static boolean overVitazaVRiadku(char[][] hraciaPlocha, char hraciZnak, char hrac, boolean vitaz){
+    public static boolean overVitazaVRiadku(char[][] hraciaPlocha, char hraciZnak, char hrac, boolean vitaz) {
         for (char[] riadok : hraciaPlocha) {
             if (riadok[0] == hraciZnak && riadok[1] == hraciZnak && riadok[2] == hraciZnak) {
                 System.out.println("Vyhral hráč: " + hrac);
@@ -102,7 +102,7 @@ public class Main {
                 if (pole[i][j] == ' ') {
                     remiza = false;
                     return remiza;
-                }else {
+                } else {
                     remiza = true;
                 }
             }
@@ -111,7 +111,7 @@ public class Main {
     }
 
     private static void vykresliPolePoVlozeniZnaku(char[][] hraciaPlocha) {
-        for (int i = 0; i < hraciaPlocha.length; i++){
+        for (int i = 0; i < hraciaPlocha.length; i++) {
             for (int j = 0; j < hraciaPlocha.length; j++) {
                 if (j == 2) {
                     System.out.print(hraciaPlocha[i][j]);
@@ -124,18 +124,18 @@ public class Main {
         }
     }
 
-    private static int[] vyberMiestoVPoli(int cislo){
+    private static int[] vyberMiestoVPoli(int cislo) {
         int[] miestoVPoli = new int[2];
-        switch (cislo){
-            case 1 -> miestoVPoli =new int[]{0,0};
-            case 2 -> miestoVPoli= new int[]{0,1};
-            case 3 -> miestoVPoli= new int[]{0,2};
-            case 4 -> miestoVPoli= new int[]{1,0};
-            case 5 -> miestoVPoli= new int[]{1,1};
-            case 6 -> miestoVPoli= new int[]{1,2};
-            case 7 -> miestoVPoli= new int[]{2,0};
-            case 8 -> miestoVPoli= new int[]{2,1};
-            case 9 -> miestoVPoli= new int[]{2,2};
+        switch (cislo) {
+            case 1 -> miestoVPoli = new int[]{0, 0};
+            case 2 -> miestoVPoli = new int[]{0, 1};
+            case 3 -> miestoVPoli = new int[]{0, 2};
+            case 4 -> miestoVPoli = new int[]{1, 0};
+            case 5 -> miestoVPoli = new int[]{1, 1};
+            case 6 -> miestoVPoli = new int[]{1, 2};
+            case 7 -> miestoVPoli = new int[]{2, 0};
+            case 8 -> miestoVPoli = new int[]{2, 1};
+            case 9 -> miestoVPoli = new int[]{2, 2};
 
         }
         return miestoVPoli;
@@ -143,7 +143,7 @@ public class Main {
 
 
     private static void naplnAVypisPole(char[][] pole) {
-        for (int i = 0; i < pole.length; i++){
+        for (int i = 0; i < pole.length; i++) {
             for (int j = 0; j < pole.length; j++) {
                 if (j == 2) {
                     pole[i][j] = ' ';
